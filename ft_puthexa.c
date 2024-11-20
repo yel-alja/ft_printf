664342e3a72b6a97b3e578e7f99b2b6d3c9cd668
yel-alja <yassine122alja@gmail.com>
2024-11-20 17:55:13 +0100

    Update ft_puthexa.c

--- ft_puthexa.c
+++ ft_puthexa.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_puthexa.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yel-alja <yel-alja@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yel-alja <yel-alja@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 09:45:20 by yel-alja          #+#    #+#             */
 /*   Updated: 2024/11/19 16:56:29 by yel-alja         ###   ########.fr       */
