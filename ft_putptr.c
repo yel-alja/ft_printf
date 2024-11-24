d352597a13d2d00591da6996d040b9bcaadfe98d
yel-alja <yassine122alja@gmail.com>
2024-11-24 20:28:05 +0100

    Update ft_putptr.c

--- ft_putptr.c
+++ ft_putptr.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putptr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yel-alja <yel-alja@student.1337.fr>        +#+  +:+       +#+        */
+/*   By: yel-alja <yel-alja@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 10:38:42 by yel-alja          #+#    #+#             */
 /*   Updated: 2024/11/21 15:32:00 by yel-alja         ###   ########.fr       */
